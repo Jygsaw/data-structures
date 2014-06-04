6704a6782d16067d97a89df34a95e828d416ff27
Hack Reactor Students <jygsaw@gmail.com>
2014-06-04 15:19:34 -0700

    Implement functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var head = 0;
+  var tail = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    if (head === 0 && tail === 0) {
+      head++;
+    }
+    tail++;
+    storage[tail] = value;
   };
 
   someInstance.dequeue = function(){
+    var result = storage[head];
+    delete storage[head];
+    head++;
+    if (head > tail) {
+      head = tail = 0;
+    }
+    return result;
   };
 
   someInstance.size = function(){
+    if (tail === 0 && tail === 0) {
+      return 0;
+    } else {
+      return tail === head ? 1 : tail - head + 1;
+    }
   };
 
   return someInstance;
