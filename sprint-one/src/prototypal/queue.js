a1e46a6b2cc91aacde9ecfc5ec96e433552ff573
Hack Reactor Students <jygsaw@gmail.com>
2014-06-04 20:43:51 -0700

    Refactor prototypal queue to streamline logic

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -9,27 +9,19 @@ var makeQueue = function() {
 var queueMethods = {};
 
 queueMethods.enqueue = function(value){
-  if (this.head === 0 && this.tail === 0) {
-    this.head++;
-  }
-  this.tail++;
   this.storage[this.tail] = value;
+  this.tail++;
 };
 
 queueMethods.dequeue = function(){
-  var result = this.storage[this.head];
-  delete this.storage[this.head];
-  this.head++;
-  if (this.head > this.tail) {
-    this.head = this.tail = 0;
+  if (this.size() > 0) {
+    var result = this.storage[this.head];
+    delete this.storage[this.head];
+    this.head++;
+    return result;
   }
-  return result;
 };
 
 queueMethods.size = function(){
-  if (this.tail === 0 && this.tail === 0) {
-    return 0;
-  } else {
-    return this.tail === this.head ? 1 : this.tail - this.head + 1;
-  }
+  return this.tail - this.head;
 };
