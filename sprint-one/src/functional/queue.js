1e8228f1136054df59e5ddbd5a938a6628f6bbe0
Hack Reactor Students <jygsaw@gmail.com>
2014-06-04 20:36:22 -0700

    Refactor functional queue to streamline logic

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,35 +3,27 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var head = 0;
   var tail = 0;
+  var head = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
-    if (head === 0 && tail === 0) {
-      head++;
-    }
-    tail++;
     storage[tail] = value;
+    tail++;
   };
 
   someInstance.dequeue = function(){
-    var result = storage[head];
-    delete storage[head];
-    head++;
-    if (head > tail) {
-      head = tail = 0;
+    if (someInstance.size() > 0) {
+      var result = storage[head];
+      delete storage[head];
+      head++;
+      return result;
     }
-    return result;
   };
 
   someInstance.size = function(){
-    if (tail === 0 && tail === 0) {
-      return 0;
-    } else {
-      return tail === head ? 1 : tail - head + 1;
-    }
+    return tail - head;
   };
 
   return someInstance;
