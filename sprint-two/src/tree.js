f163e2dbc0722c340d69b9df4096eb50fd29c4b7
Hack Reactor Students <jygsaw@gmail.com>
2014-06-05 20:29:30 -0700

    Implement simple tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,20 +2,40 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  // check for children array
+  if (!this.children) {
+    // initialize children array
+    this.children = [];
+  }
 
+  // add new child to children array
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  // check own value
+  if (this.value === target) {
+    return true;
+  }
 
+  // recursively check children
+  if (this.children) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+
+  // return false if target not found
+  return false;
 };
 
 
