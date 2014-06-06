a8497dc82417dbe8f9274ee29323160bee1f6e07
Hack Reactor Students <jygsaw@gmail.com>
2014-06-05 20:02:57 -0700

    Implement hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,16 +4,47 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  // init vars
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var targetBucket = this._storage.get(i);
+
+  // checking if bucket exists
+  if(!targetBucket){
+    // creating new bucket
+    targetBucket = {};
+    this._storage.set(i,targetBucket);
+  }
+
+  // setting key/val in bucket
+  targetBucket[k] = v;
+
 };
 
 HashTable.prototype.retrieve = function(k){
+  // init vars
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var targetBucket = this._storage.get(i);
 
+  // checking if key/val exists
+  if(targetBucket[k]){
+    // return key/val
+    return targetBucket[k];
+  }
+
+  // return null if nothing found
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
+  // init vars
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var targetBucket = this._storage.get(i);
 
+  // checking if target bucket exists
+  if(targetBucket){
+    // delete key/val from bucket
+    delete targetBucket[k];
+  }
 };
 
 
